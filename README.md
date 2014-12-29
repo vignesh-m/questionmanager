836806b8a94a1aabcaaa28f9ae9b413d83056df1
vignesh-m <vicky.mano@gmail.com>
2014-12-29 23:46:54 +0530

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# questionmanager
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/QuestionsController.js
@@ -0,0 +1,11 @@
+/**
+ * QuestionsController
+ *
+ * @description :: Server-side logic for managing questions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
